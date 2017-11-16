6d9310c654ff6221e05550100a46c0a630624155
student <student@DESKTOP-N5SQ7JP>
2017-11-16 12:26:31 +0100

    WindowsUserAdministration

---
+++ UserAdministration/WindowsFormsUsers/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DatabaseService;  // koristimo servis
+
+namespace WindowsFormsUsers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Crud Crud = new Crud();
+            List <User> lUsers = Crud.GetUsers();
+            dataGridViewUsers.DataSource = lUsers;
+
+            // gumb za azuriranje korisnika
+            DataGridViewImageColumn oEditButton = new DataGridViewImageColumn();   
+            oEditButton.Image = Image.FromFile("D:/DomagojBodo/icon.png");
+            oEditButton.Width = 20;
+            oEditButton.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            dataGridViewUsers.Columns.Add(oEditButton);
+
+            // za brisanje korisnika
+            DataGridViewImageColumn oDeleteButton = new DataGridViewImageColumn();
+            oDeleteButton.Image = Image.FromFile("D:/DomagojBodo/icon2.png");
+            oDeleteButton.Width = 20;
+            oDeleteButton.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            dataGridViewUsers.Columns.Add(oDeleteButton);
+
+            dataGridViewUsers.AutoGenerateColumns = false;
+        }
+
+        private void dataGridViewUsers_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            dataGridViewUsers.Rows[e.RowIndex].Selected = true;
+            if (dataGridViewUsers.CurrentCell.ColumnIndex.Equals(5) && e.RowIndex != -1)
+            {
+                FormEditUser FormEditUser = new FormEditUser(this);
+                FormEditUser.lblEditUserID.Text = dataGridViewUsers.Rows[e.RowIndex].Cells[0].Value.ToString();
+                FormEditUser.lblEditUserName.Text = dataGridViewUsers.Rows[e.RowIndex].Cells[1].Value.ToString();
+                FormEditUser.inptEditPassword.Text = dataGridViewUsers.Rows[e.RowIndex].Cells[2].Value.ToString();
+                FormEditUser.inptEditName.Text = dataGridViewUsers.Rows[e.RowIndex].Cells[3].Value.ToString();
+                FormEditUser.inptEditSurname.Text = dataGridViewUsers.Rows[e.RowIndex].Cells[4].Value.ToString();            
+                FormEditUser.Show();
+            }
+        }
+
+    }
+}
+
---
+++ UserAdministration/WindowsFormsUsers/Form2.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace WindowsFormsUsers
+{
+    partial class FormEditUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEditUserName = new System.Windows.Forms.Label();
+            this.lblEditTitle = new System.Windows.Forms.Label();
+            this.lblEditUserID = new System.Windows.Forms.Label();
+            this.lblEditName = new System.Windows.Forms.Label();
+            this.lblEditLastName = new System.Windows.Forms.Label();
+            this.lblEditPassword = new System.Windows.Forms.Label();
+            this.inptEditName = new System.Windows.Forms.TextBox();
+            this.inptEditPassword = new System.Windows.Forms.TextBox();
+            this.inptEditSurname = new System.Windows.Forms.TextBox();
+            this.btnEditCancel = new System.Windows.Forms.Button();
+            this.btnEditUser = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblEditUserName
+            // 
+            this.lblEditUserName.AutoSize = true;
+            this.lblEditUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblEditUserName.Location = new System.Drawing.Point(176, 46);
+            this.lblEditUserName.Name = "lblEditUserName";
+            this.lblEditUserName.Size = new System.Drawing.Size(75, 17);
+            this.lblEditUserName.TabIndex = 0;
+            this.lblEditUserName.Text = "UserName";
+            // 
+            // lblEditTitle
+            // 
+            this.lblEditTitle.AutoSize = true;
+            this.lblEditTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.lblEditTitle.Location = new System.Drawing.Point(149, 22);
+            this.lblEditTitle.Name = "lblEditTitle";
+            this.lblEditTitle.Size = new System.Drawing.Size(133, 24);
+            this.lblEditTitle.TabIndex = 1;
+            this.lblEditTitle.Text = "Uredi korisnika";
+            // 
+            // lblEditUserID
+            // 
+            this.lblEditUserID.AutoSize = true;
+            this.lblEditUserID.Location = new System.Drawing.Point(193, 63);
+            this.lblEditUserID.Name = "lblEditUserID";
+            this.lblEditUserID.Size = new System.Drawing.Size(40, 13);
+            this.lblEditUserID.TabIndex = 2;
+            this.lblEditUserID.Text = "UserID";
+            this.lblEditUserID.Visible = false;
+            // 
+            // lblEditName
+            // 
+            this.lblEditName.AutoSize = true;
+            this.lblEditName.Location = new System.Drawing.Point(48, 117);
+            this.lblEditName.Name = "lblEditName";
+            this.lblEditName.Size = new System.Drawing.Size(24, 13);
+            this.lblEditName.TabIndex = 3;
+            this.lblEditName.Text = "Ime";
+            // 
+            // lblEditLastName
+            // 
+            this.lblEditLastName.AutoSize = true;
+            this.lblEditLastName.Location = new System.Drawing.Point(28, 144);
+            this.lblEditLastName.Name = "lblEditLastName";
+            this.lblEditLastName.Size = new System.Drawing.Size(44, 13);
+            this.lblEditLastName.TabIndex = 4;
+            this.lblEditLastName.Text = "Prezime";
+            // 
+            // lblEditPassword
+            // 
+            this.lblEditPassword.AutoSize = true;
+            this.lblEditPassword.Location = new System.Drawing.Point(28, 170);
+            this.lblEditPassword.Name = "lblEditPassword";
+            this.lblEditPassword.Size = new System.Drawing.Size(44, 13);
+            this.lblEditPassword.TabIndex = 5;
+            this.lblEditPassword.Text = "Lozinka";
+            // 
+            // inptEditName
+            // 
+            this.inptEditName.Location = new System.Drawing.Point(100, 110);
+            this.inptEditName.Name = "inptEditName";
+            this.inptEditName.Size = new System.Drawing.Size(240, 20);
+            this.inptEditName.TabIndex = 6;
+            // 
+            // inptEditPassword
+            // 
+            this.inptEditPassword.Location = new System.Drawing.Point(100, 163);
+            this.inptEditPassword.Name = "inptEditPassword";
+            this.inptEditPassword.Size = new System.Drawing.Size(240, 20);
+            this.inptEditPassword.TabIndex = 8;
+            // 
+            // inptEditSurname
+            // 
+            this.inptEditSurname.Location = new System.Drawing.Point(100, 137);
+            this.inptEditSurname.Name = "inptEditSurname";
+            this.inptEditSurname.Size = new System.Drawing.Size(240, 20);
+            this.inptEditSurname.TabIndex = 9;
+            // 
+            // btnEditCancel
+            // 
+            this.btnEditCancel.Location = new System.Drawing.Point(100, 213);
+            this.btnEditCancel.Name = "btnEditCancel";
+            this.btnEditCancel.Size = new System.Drawing.Size(85, 34);
+            this.btnEditCancel.TabIndex = 10;
+            this.btnEditCancel.Text = "ZATVORI";
+            this.btnEditCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnEditUser
+            // 
+            this.btnEditUser.Location = new System.Drawing.Point(249, 213);
+            this.btnEditUser.Name = "btnEditUser";
+            this.btnEditUser.Size = new System.Drawing.Size(91, 34);
+            this.btnEditUser.TabIndex = 11;
+            this.btnEditUser.Text = "SPREMI";
+            this.btnEditUser.UseVisualStyleBackColor = true;
+            this.btnEditUser.Click += new System.EventHandler(this.btnEditUser_Click);
+            // 
+            // FormEditUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 308);
+            this.Controls.Add(this.btnEditUser);
+            this.Controls.Add(this.btnEditCancel);
+            this.Controls.Add(this.inptEditSurname);
+            this.Controls.Add(this.inptEditPassword);
+            this.Controls.Add(this.inptEditName);
+            this.Controls.Add(this.lblEditPassword);
+            this.Controls.Add(this.lblEditLastName);
+            this.Controls.Add(this.lblEditName);
+            this.Controls.Add(this.lblEditUserID);
+            this.Controls.Add(this.lblEditTitle);
+            this.Controls.Add(this.lblEditUserName);
+            this.Name = "FormEditUser";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label lblEditUserName;
+        private System.Windows.Forms.Label lblEditTitle;
+        public System.Windows.Forms.Label lblEditUserID;
+        private System.Windows.Forms.Label lblEditName;
+        private System.Windows.Forms.Label lblEditLastName;
+        private System.Windows.Forms.Label lblEditPassword;
+        public System.Windows.Forms.TextBox inptEditName;
+        public System.Windows.Forms.TextBox inptEditPassword;
+        public System.Windows.Forms.TextBox inptEditSurname;
+        private System.Windows.Forms.Button btnEditCancel;
+        private System.Windows.Forms.Button btnEditUser;
+    }
+}
